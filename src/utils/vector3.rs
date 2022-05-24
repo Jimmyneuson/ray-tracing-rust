4bf486bd62b6f98428896a32e0d78516e77c96aa
Anh Luong <albertsrift@gmail.com>
2022-05-24 06:55:10 -0700

    add vector3 class

---
+++ src/utils/vector3.rs
@@ -0,0 +1,163 @@
+use std::ops;
+
+#[derive(PartialEq, Clone, Copy)]
+struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vector3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+    
+    pub fn magnitude(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+    
+    pub fn dot(&self, other: Vector3) -> f64 {
+          self.x * other.x
+        + self.y * other.y
+        + self.z * other.z
+    }
+    
+    pub fn cross(&mut self, other: Vector3) -> Self {
+        Self {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+    
+    pub fn unit(&mut self) -> Self {
+        *self / self.magnitude()
+    }
+}
+
+impl ops::Neg for Vector3 {
+    type Output = Self; 
+    
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl ops::Add for Vector3 {
+    type Output = Self;
+    
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        } 
+    }
+}
+
+impl ops::Mul<f64> for Vector3 {
+    type Output = Self;
+    
+    fn mul(self, scalar: f64) -> Self {
+        Self {
+            x: self.x * scalar,         
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }  
+    }
+}
+
+impl ops::Div<f64> for Vector3 {
+    type Output = Self;
+    
+    fn div(self, scalar: f64) -> Self {
+        Self {
+            x: self.x / scalar,         
+            y: self.y / scalar,
+            z: self.z / scalar,
+        }  
+    }
+}
+
+impl std::fmt::Debug for Vector3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; 
+    
+    #[test]
+    fn negate_vector() {
+        let v = Vector3::new(1.0, 2.0, 3.0);
+        assert_eq!(-v, Vector3::new(-1.0, -2.0, -3.0));
+    }
+    
+    #[test]
+    fn add_vector() {
+        let v1 = Vector3::new(1.0, 1.0, 1.0);
+        let v2 = Vector3::new(2.0, 2.0, 2.0);
+        assert_eq!(v1 + v2, Vector3::new(3.0, 3.0, 3.0));
+    }
+    
+    #[test]
+    fn scalar_mul_vector() {
+        let v = Vector3::new(1.0, 2.0, 3.0);
+        let s = 2.0;
+        assert_eq!(v * s, Vector3::new(2.0, 4.0, 6.0));
+    }
+
+    #[test]
+    fn div_vector() {
+        let v = Vector3::new(1.0, 2.0, 3.0);
+        let s = 2.0;
+        assert_eq!(v / s, Vector3::new(0.5, 1.0, 1.5));
+    }
+    
+    #[test]
+    fn magnitude_vector() {
+        let v = Vector3::new(1.0, 2.0, 3.0);
+        assert_eq!(v.magnitude(), 14f64.sqrt());
+    }
+    
+    #[test]
+    fn dot_vector() {
+        let v1 = Vector3::new(1.0, 2.0, 3.0);
+        let v2 = Vector3::new(1.0, 2.0, 3.0);
+        assert_eq!(v1.dot(v2), 14.0);
+    }
+    
+    #[test]
+    fn cross_vector() {
+        let mut v1 = Vector3::new(1.0, 2.0, 3.0);
+        let v2 = Vector3::new(1.0, 4.0, 1.0);
+        assert_eq!(v1.cross(v2), Vector3::new(-10.0, 2.0, 2.0));
+    }
+    
+    #[test]
+    fn cross_vector_with_itself() {
+        let mut v1 = Vector3::new(1.0, 2.0, 3.0);
+        assert_eq!(v1.cross(v1), Vector3::new(0.0, 0.0, 0.0));
+    }
+    
+    #[test]
+    fn unit_vector() {
+        let mut v = Vector3::new(1.0, 2.0, 3.0); 
+        assert_eq!(v.unit(), Vector3::new(
+            1.0/14f64.sqrt(), 
+            (2.0/7.0f64).sqrt(), 
+            3.0/14f64.sqrt()));
+    }
+    
+    #[test]
+    fn unit_vector_length_is_one() {
+        let mut v = Vector3::new(1.0, 2.0, 3.0); 
+        assert_eq!(v.unit().magnitude(), 1.0);
+    }
+}
