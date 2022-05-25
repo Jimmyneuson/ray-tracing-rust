8c27048758f3a41f346657f5265dae7cc470eb71
Anh Luong <albertsrift@gmail.com>
2022-05-24 19:05:33 -0700

    fix sky test and clean

--- tests/render.rs
+++ tests/render.rs
@@ -6,6 +6,8 @@ use utils::ppm::RGBTriplet;
 use utils::camera::Camera;
 use utils::ray::Ray;
 
+use std::fs::read_to_string;
+
 use itertools::iproduct;
     
 #[test]
@@ -17,6 +19,7 @@ fn sky() {
     let origin = Vector3::new(0.0, 0.0, 0.0);
     
     let mut ppm = PPM::new(image_width, image_height);
+    let cmp = read_to_string("./tests/sky.ppm").unwrap();
     
     let camera = Camera::new(2.0, aspect_ratio * 2.0, 1.0, origin);
     
@@ -25,7 +28,7 @@ fn sky() {
                           - camera.vertical / 2.0
                           - Vector3::new(0.0, 0.0, camera.focal_length);
     
-    for (j, i) in iproduct!((0..image_height).rev(), 0..image_height) {
+    for (j, i) in iproduct!((0..image_height).rev(), 0..image_width) {
         let u = i as f64 / (image_width - 1) as f64;
         let v = j as f64 / (image_height - 1) as f64;
         let mut r = Ray::new(camera.position, lower_left_corner
@@ -37,7 +40,8 @@ fn sky() {
         let t = 0.5 * (unit_direction.y + 1.0);
         let v = Vector3::lerp(&Vector3::new(1.0, 1.0, 1.0),
                               &Vector3::new(0.5, 0.7, 1.0), t).color();
-        ppm.set(i, image_height - j - 1,
-                RGBTriplet::new(v.x as u8, v.y as u8, v.z as u8));
+        ppm.set(i, image_height - j - 1, RGBTriplet::new(v.x as u8, v.y as u8, v.z as u8));
     }
+    
+    assert_eq!(ppm.to_string(), cmp);
 }
